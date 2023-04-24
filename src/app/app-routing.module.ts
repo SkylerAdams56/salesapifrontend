8f2a1219059c001e85370102e228bcf358b46050
Skyler Adams <skyler14adams@gmail.com>
2023-04-24 13:29:28 -0400

    Created the employee table and all its functions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component'
+import { EmployeeChangeComponent } from './employee/employee-change/employee-change.component';
+import { EmployeeCreateComponent } from './employee/employee-create/employee-create.component';
+import { EmployeeDetailComponent } from './employee/employee-detail/employee-detail.component';
+import { EmployeeLoginComponent } from './employee/employee-login/employee-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: "", redirectTo: "/employee/list", pathMatch: "full" },
+  { path: "employee/list", component: EmployeeListComponent },
+  { path: "employee/change/:id", component: EmployeeChangeComponent },
+  { path: "employee/create", component: EmployeeCreateComponent },
+  { path: "employee/detail/:id", component: EmployeeDetailComponent },
+  { path: "employee/login", component: EmployeeLoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/employee/employee-login/employee-login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employee-login',
+  templateUrl: './employee-login.component.html',
+  styleUrls: ['./employee-login.component.css']
+})
+export class EmployeeLoginComponent {
+
+  pageTitle = "Employee Login";
+  email:string= "";
+  password: string= "";
+  message: string= "";
+  constructor(
+    private emplSvc: EmployeeService,
+    private router: Router
+    
+  ) {}
+
+  login(): void {
+    this.emplSvc.login(this.email, this.password).subscribe({
+      next: (res) => {
+        console.debug("Login Successful");
+        this.router.navigateByUrl("/employee/list");
+    },
+    error: (err) => {
+      console.error(err);
+    }
+    
+    })
+  }
+
+  ngOnInit():void{
+
+  }
+
+}
